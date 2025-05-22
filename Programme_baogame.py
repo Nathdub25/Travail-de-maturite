6acc89fdf3ade89e576fff6c2467414c46220781
Nathdub25 <nathan.dubel@edu.gbjb.ch>
2025-05-22 21:35:05 +0200

    Add files via upload

--- Programme_baogame.py
+++ Programme_baogame.py
@@ -29,7 +29,7 @@ while z < 420:
 
 jeu = [[1]*8 for i in range(0,4)]
 print (jeu)
-tableau = [[]*8 for i in range(0,4)]
+tableau = [[None]*8 for i in range(0,4)]
 #print (tableau[0][0])
 
 
@@ -37,7 +37,7 @@ for j in range (4):
     for i in range (len(jeu[0])):
         valcer = can.create_text(i*100 + 40,j*100 + 40, text = str(jeu[j][i]))
         print (j, i)
-        tableau[j][i].append(valcer)
+        tableau[j][i] = valcer
         print (tableau[j][i])
 
 def adgr(event):
@@ -45,20 +45,56 @@ def adgr(event):
     clic_x = event.x
     clic_y = event.y 
     
-    li = clic_x // 100 
-    co = clic_y // 100 
-        
-    jeu[li][co] = 0
-    jeu[li][co+1] = jeu[li][co+1] + 1
-    can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
-    can.itemconfig(tableau[li][co + 1],text = str(jeu[li][co + 1]))
+    co = clic_x // 100 
+    li = clic_y // 100 
+    if clic_y > 240 and clic_y < 340 and clic_x > 140:
+        jeu[li][co-1] = jeu[li][co-1] + jeu[li][co]    
+        jeu[li][co] = jeu[li][co]-jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li][co - 1],text = str(jeu[li][co - 1]))
+    if clic_x > 0 and clic_x < 140 and clic_y > 240 and clic_y < 340:
+        jeu[li+1][co] = jeu[li+1][co] + jeu[li][co]
+        jeu[li][co] = jeu[li][co] - jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li+1][co],text = str(jeu[li+1][co]))
+    if clic_y > 340 and clic_y < 440 and clic_x < 740:
+        jeu[li][co+1] = jeu[li][co+1] + jeu[li][co]    
+        jeu[li][co] = jeu[li][co]-jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li][co + 1],text = str(jeu[li][co + 1]))
+    if clic_x > 740 and clic_x < 840 and clic_y > 340 and clic_y < 440:
+        jeu[li-1][co] = jeu[li-1][co] + jeu[li][co]
+        jeu[li][co] = jeu[li][co] - jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li-1][co],text = str(jeu[li-1][co]))
+    if clic_y > 140 and clic_y < 240 and clic_x < 740:
+        jeu[li][co+1] = jeu[li][co+1] + jeu[li][co]    
+        jeu[li][co] = jeu[li][co]-jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li][co + 1],text = str(jeu[li][co + 1]))
+    if clic_x > 740 and clic_x < 840 and clic_y > 140 and clic_y < 240:
+        jeu[li-1][co] = jeu[li-1][co] + jeu[li][co]
+        jeu[li][co] = jeu[li][co] - jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li-1][co],text = str(jeu[li-1][co]))
+    if clic_y > 0 and clic_y < 140 and clic_x > 140:
+        jeu[li][co-1] = jeu[li][co-1] + jeu[li][co]    
+        jeu[li][co] = jeu[li][co]-jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li][co - 1],text = str(jeu[li][co - 1]))
+    if clic_x > 0 and clic_x < 140 and clic_y > 0 and clic_y < 140:
+        jeu[li+1][co] = jeu[li+1][co] + jeu[li][co]
+        jeu[li][co] = jeu[li][co] - jeu[li][co]
+        can.itemconfig(tableau[li][co],text = str(jeu[li][co]))
+        can.itemconfig(tableau[li+1][co],text = str(jeu[li+1][co]))
+
 
     
     
     
     
     
-fen.bind('<Button-1>', adgr)
+can.bind('<Button-1>', adgr)
     
     
     
